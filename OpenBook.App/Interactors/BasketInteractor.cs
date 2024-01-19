51b1bf6b89da455d144272aca462dc8150956302
glimnurovaidar <gaaidar@mail.ru>
2024-01-19 19:39:24 +0500

    Addede basket interactor

---
+++ OpenBook.App/Interactors/BasketInteractor.cs
@@ -0,0 +1,171 @@
+﻿using OpenBook.App.Data.Transaction;
+using OpenBook.App.Mappers;
+using OpenBook.App.Storage;
+using OpenBook.Domain.Entity;
+using OpenBook.Shared.Dtos;
+using OpenBook.Shared.OutputData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBook.App.Interactors
+{
+    public class BasketInteractor
+    {
+        private IBasketRepository basketRepository;
+        private IBookRepository bookBasketRepository;
+        private IUnitWork unitWork;
+
+        public BasketInteractor(IBasketRepository basketRepository, IBookRepository bookBasketRepository,
+            IUnitWork unitWork)
+        {
+            this.basketRepository = basketRepository;
+            this.bookBasketRepository = bookBasketRepository;
+            this.unitWork = unitWork;
+        }
+        public async Task<Response> CreateWithEntity(BasketDto basket)
+        {
+            var response = new Response<BasketDto>();
+            try
+            {
+                await basketRepository.CreateWithEntity(basket.ToEntity());
+                response.IsSuccess = true;
+                await unitWork.Commit();
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Внутренняя ошибка!";
+                response.IsSuccess = false;
+                response.ErrorInfo = ex.Message;
+            }
+            return response;
+        }
+        public async Task<Response<BasketDto>> Read(int id)
+        {
+            var response = new Response<BasketDto>();
+            try
+            {
+                var basket = await basketRepository.Read(id);
+                response.Value = basket.ToDto();
+                response.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Внутренняя ошибка!";
+                response.IsSuccess = false;
+                response.ErrorInfo = ex.Message;
+            }
+            return response;
+        }
+        public async Task<Response> UpdateWithEntity(BasketDto basket)
+        {
+            var response = new Response<BasketDto>();
+            try
+            {
+                await basketRepository.UpdateWithEntity(basket.ToEntity());
+                response.IsSuccess = true;
+                await unitWork.Commit();
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Внутренняя ошибка!";
+                response.IsSuccess = false;
+                response.ErrorInfo = ex.Message;
+            }
+            return response;
+        }
+        public async Task<Response> Delete(int id)
+        {
+            var response = new Response<BasketDto>();
+            try
+            {
+                await basketRepository.Delete(id);
+                response.IsSuccess = true;
+                await unitWork.Commit();
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Внутренняя ошибка!";
+                response.IsSuccess = false;
+                response.ErrorInfo = ex.Message;
+            }
+            return response;
+        }
+        public async Task<Response> Create(int userId)
+        {
+            var response = new Response<BasketDto>();
+            try
+            {
+                await basketRepository.Create(userId);
+                response.IsSuccess = true;
+                await unitWork.Commit();
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Внутренняя ошибка!";
+                response.IsSuccess = false;
+                response.ErrorInfo = ex.Message;
+            }
+            return response;
+        }
+        public async Task<Response> AddBook(int userId, int bookId)
+        {
+            var response = new Response<BasketDto>();
+            try
+            {
+                await basketRepository.AddBook(userId, bookId);
+                response.IsSuccess = true;
+                await unitWork.Commit();
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Внутренняя ошибка!";
+                response.IsSuccess = false;
+                response.ErrorInfo = ex.Message;
+            }
+            return response;
+        }
+        public async Task<Response> RemoveBook(int userId, int bookId)
+        {
+            var response = new Response<BasketDto>();
+            try
+            {
+                await basketRepository.RemoveBook(userId, bookId);
+                response.IsSuccess = true;
+                await unitWork.Commit();
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Внутренняя ошибка!";
+                response.IsSuccess = false;
+                response.ErrorInfo = ex.Message;
+            }
+            return response;
+        }
+        public async Task<Response<IEnumerable<BookDto>>> GetBooks(int userId, int start, int count)
+        {
+            var response = new Response<IEnumerable<BookDto>>();
+            try
+            {
+                var data = basketRepository.GetBooks(userId, start, count);
+
+                List<BookDto> books = new();
+                await foreach (var item in data)
+                {
+                    books.Add(item.ToDto());
+                }
+                response.Value = books.ToList();
+                response.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Внутренняя ошибка!";
+                response.IsSuccess = false;
+                response.ErrorInfo = ex.Message;
+            }
+            return response;
+        }
+    }
+}
