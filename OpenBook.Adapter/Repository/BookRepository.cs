c7aa9f82e0a5b0e1b13775d6f348a8926d9304d7
glimnurovaidar <gaaidar@mail.ru>
2024-03-15 20:55:51 +0500

    Delete cycle from book

--- OpenBook.Adapter/Repository/BookRepository.cs
+++ OpenBook.Adapter/Repository/BookRepository.cs
@@ -133,10 +133,15 @@ namespace OpenBook.Adapter.Repository
         {
             var user = await context.Users.FirstOrDefaultAsync(u => u.Id == book.UserId);
             book.User = user;
-            if (book.CycleId != null)
+            if (book.CycleId != null && book.CycleId != 0)
             {
                 var cycle = await context.Cycles.FirstOrDefaultAsync(c => c.Id == book.CycleId);
                 book.Cycle = cycle;
+            }
+            if (book.CycleId == 0)
+            {
+                book.CycleId = null;
+                book.Cycle = null;
             }
             context.Update(book);
         }
