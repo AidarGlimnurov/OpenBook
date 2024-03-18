2acf84e2ee49edd26133d5725204ddd4dd9121eb
glimnurovaidar <gaaidar@mail.ru>
2024-03-18 20:41:10 +0500

    Trying pdf downloader

---
+++ OpenBook/Client/wwwroot/interop.js
@@ -0,0 +1,8 @@
+﻿window.downloadFile = function (fileName, filePath) {
+    var anchor = document.createElement('a');
+    anchor.href = window.URL.createObjectURL(new Blob([filePath], { type: 'application/octet-stream' }));
+    anchor.download = fileName;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+};
