e6a80509ec60f97ee841239c51ada0651967e401
glimnurovaidar <gaaidar@mail.ru>
2024-03-19 19:09:38 +0500

    Kirilliza trouble, but download is done

--- OpenBook/Server/Controllers/ChapterController.cs
+++ OpenBook/Server/Controllers/ChapterController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OpenBook.Adapter.Repository;
 using OpenBook.Adapter.Transaction;
 using OpenBook.App.Interactors;
 using OpenBook.Shared.Dtos;
 using OpenBook.Shared.OutputData;
+using OpenBook.Shared.SupportData;
+using System.Diagnostics.Metrics;
 
 namespace OpenBook.Server.Controllers
 {
@@ -56,6 +61,31 @@ namespace OpenBook.Server.Controllers
         public async Task<Response<DataPage<ChapterDto>>> GetForBook(int bookId, int start, int? count, bool? isPublic)
         {
             return await interactor.GetForBook(bookId, start, count, isPublic);
+        }
+        [HttpGet("DownloadFile")]
+        public async Task<ActionResult> DownloadFile(string data, string name)
+        {
+            var value = await GeneratePDF(data);
+            return File(value, "application/octet-stream", name);
+        }
+        private async Task<byte[]> GeneratePDF(string value)
+        {
+            // Создаем документ PDF
+            Document document = new Document();
+            MemoryStream memoryStream = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
+            document.Open();
+
+            // Преобразуем HTML в PDF
+            StringReader stringReader = new StringReader(value);
+            HTMLWorker htmlWorker = new HTMLWorker(document);
+            htmlWorker.Parse(stringReader);
+
+            // Закрываем документ
+            document.Close();
+
+            // Возвращаем PDF в виде массива байтов
+            return memoryStream.ToArray();
         }
     }
 }
---
+++ OpenBook/Shared/SupportData/PdfStreamData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBook.Shared.SupportData
+{
+    public class PdfStreamData
+    {
+        public byte[] Data { get; set; }
+        public string Name{ get; set; }
+    }
+}
